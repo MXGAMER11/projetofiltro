b4c25db30e01cc0449aec455386b045868dff988
MXGAMER11 <118226725+MXGAMER11@users.noreply.github.com>
2023-08-15 19:49:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,14 +4,38 @@ noseY=0;
 
 
 function preload(){
-
+bigode = loadImage("https://postimg.cc/bsV5tR30")
 }
 
 function setup() {
   canvas = createCanvas(300,300)
   canvas.center();
+  video = createCapture(VIDEO)
+  video.size(300,300)
+  video.hide()
+  poseNet = ml5.poseNet(video,modelLoaded)
+ poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet foi inicializado');
 }
 
 function draw() {
+image(video,0,0,300,300)
+image(bigode, noseX, noseY, 80, 35);
+}
 
+function takeSnapshot(){
+  save("suafoto🗿🍷.png")
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-40;
+    noseY = results[0].pose.nose.y;
+  }
 }
